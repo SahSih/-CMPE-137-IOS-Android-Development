3715b962c6ca5a6598aeaf8a7ab3b0df69069263
Chris <chrispeng9371@gmail.com>
2017-02-06 20:42:48 -0800

    Update App Testing

---
+++ HealthyDayTests/HealthyDayTests.swift
@@ -0,0 +1,21 @@
+//
+//  HealthDayTests.swift
+//  HealthyDay
+//
+//  Created by Group 5 (MJD, NL, CP) on 10/8/16.
+//  Copyright © 2016 San Jose State University. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import HealthyDay
+
+class MainViewControllerTests: QuickSpec{
+    override func spec(){
+        describe("test"){
+            it("work"){
+                expect(1+1).to(equal(2))
+            }
+        }
+    }
+}
